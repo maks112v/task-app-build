129ea89c1e81143e61be0421eeacf46f0e9bd21f
Maksim V <maks112v@gmail.com>
2020-07-25 08:17:28 -0400

    working on stuff

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
+  const tasks = ["do laundry", "clean car", "learn html"];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input />
+      <ul>
+        {tasks.map((taskName) => (
+          <li>{taskName}</li>
+        ))}
+      </ul>
     </div>
   );
 }
